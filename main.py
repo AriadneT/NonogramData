d94f06c71c769568dba85470ddec0c28c8d1a4b6
AriadneT <AriadneT@users.noreply.github.com>
2020-02-06 22:22:26 +0100

    Use DataGroup class to simplify data collection
    Plot all four data groups. Store linear regression in a file.

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from scipy import stats
 import mysql.connector
 from mysql.connector import Error
 import json
-#import os
 import matplotlib.pyplot as plotter
 from Classes.DataGroup import DataGroup
 
 # Comparisons of data with different numbers of placings
 # e.g. x35 and y35 have placings 1 to 35
-x35 = []
-y35 = []
-x25 = []
-y25 = []
+x35 = DataGroup("x", 35)
+y35 = DataGroup("y", 35)
+x25 = DataGroup("x", 25)
+y25 = DataGroup("y", 25)
 x30 = DataGroup("x", 30)
-y30 = []
-x20 = []
-y20 = []
+y30 = DataGroup("y", 30)
+x20 = DataGroup("x", 20)
+y20 = DataGroup("y", 20)
+DataGroups = [x35, y35, x25, y25, x30, y30, x20, y20]
 config = []
 
 with open('Configuration/config.json') as config_file:
@@ -43,32 +43,21 @@ try:
         # Entering data via .csv files when table has foreign key does not work
         # unless foreign key ignored, which is not desired
 
-        # Lines vary from 20 to 35 in length, so limit results to first 20 solved
-        #db_interaction.execute("SELECT succession FROM data WHERE succession < 21")
-        db_interaction.execute("SELECT succession FROM data JOIN line ON data.line_id = line.line_id WHERE places = 35")
-        order_results = db_interaction.fetchall()
-        for order in order_results:
-            y35.append(order[0])
-
-        second_db_interaction = db_connection.cursor()
-        # For numbers below 1, multiply by 100 to allow stats.lineregress to work.
-        # Because line lengths vary and therefore affect maximum block size,
-        # correct for length. Log transformation seems to be more suitable.
-        #db_interaction.execute("SELECT log(longest_block / length) * 100 FROM data JOIN line ON data.line_id = line.line_id WHERE succession < 21")
-        db_interaction.execute("SELECT log(longest_block / length) * 100 FROM data JOIN line ON data.line_id = line.line_id WHERE places = 35")
-        longest_block_results = db_interaction.fetchall()
-        for longest_block in longest_block_results:
-            x35.append(int(longest_block[0]))
-
-        db_interaction.execute("SELECT succession FROM data JOIN line ON data.line_id = line.line_id WHERE places = 25")
-        order_results = db_interaction.fetchall()
-        for order in order_results:
-            y25.append(order[0])
-
-        db_interaction.execute("SELECT log(longest_block / length) * 100 FROM data JOIN line ON data.line_id = line.line_id WHERE places = 25")
-        longest_block_results = db_interaction.fetchall()
-        for longest_block in longest_block_results:
-            x25.append(int(longest_block[0]))
+        # Lines vary from 20 to 35 in length
+        for data_group in DataGroups:
+            if data_group.type == "y":
+                db_interaction.execute("SELECT succession FROM data JOIN line ON data.line_id = line.line_id WHERE places = " + str(data_group.spaces))
+                order_results = db_interaction.fetchall()
+                for order in order_results:
+                    data_group.data.append(order[0])
+            else:
+                # For numbers below 1, multiply by 100 to allow stats.lineregress to work.
+                # Because line lengths vary and therefore affect maximum block size,
+                # correct for length. Log transformation seems to be more suitable.
+                db_interaction.execute("SELECT log(longest_block / length) * 100 FROM data JOIN line ON data.line_id = line.line_id WHERE places = " + str(data_group.spaces))
+                longest_block_results = db_interaction.fetchall()
+                for longest_block in longest_block_results:
+                    data_group.data.append(int(longest_block[0]))
 except Error as error:
     print("Error while connecting to MySQL", error)
 finally:
@@ -77,11 +66,10 @@ finally:
 
 """
 # Test storing data in file and extracting them
-if os.path.isfile('x_values.txt'):
-    with open('x_values.txt', 'w') as x_file:
-        for number in x:
-            entry = str(number) + " "
-            x_file.write(entry)
+with open('x_values.txt', 'w') as x_file:
+    for number in x:
+        entry = str(number) + " "
+        x_file.write(entry)
 x_read = []
 with open('x_values.txt', 'r') as x_file:
     for line in x_file:
@@ -91,37 +79,30 @@ with open('x_values.txt', 'r') as x_file:
             x_read.append(int(number))
 """
 # Linear regression analysis
-slope35, intercept35, r_value35, p_value35, std_err35 = stats.linregress(x35,y35)
+slope35, intercept35, r_value35, p_value35, std_err35 = stats.linregress(x35.data,y35.data)
 
+with open('scatter_plot.txt', 'w') as data_file:
+    data_file.write("When 35 lines must be filled:\n\n")
+    data_file.write("order = " + str(slope35) + "log(longest_block / length) x 100 + " + str(intercept35) + "\n")
+    data_file.write("R-squared: " + str(r_value35**2) + "\n")
+    data_file.write("P-value: " + str(p_value35) + "\n\n")
+"""
 print ("When 35 lines must be filled:")
 print ("order =", slope35 ,"log(longest_block / length) x 100 +", intercept35)
 print ("R-squared:", r_value35**2)
 print ("P-value:", p_value35)
-
-"""
-plotter.scatter(x35, y35, c="black")
-plotter.title('Scatter plot')
-plotter.xlabel('log(longest_block / length) x 100')
-plotter.ylabel('order')
-plotter.show()
 """
 # Second linear regression analysis
-slope25, intercept25, r_value25, p_value25, std_err25 = stats.linregress(x25,y25)
+slope25, intercept25, r_value25, p_value25, std_err25 = stats.linregress(x25.data,y25.data)
 
 print ("When 25 lines must be filled:")
 print ("order =", slope25 ,"log(longest_block / length) x 100 +", intercept25)
 print ("R-squared:", r_value25**2)
 print ("P-value:", p_value25)
-"""
-plotter.scatter(x25, y25, c="red")
-plotter.title('Scatter plot')
-plotter.xlabel('log(longest_block / length) x 100')
-plotter.ylabel('order')
-plotter.show()
-"""
-data = ((x35,y35),(x25,y25),(x20,y20))
-colors = ("red", "black","green")
-groups = ("35 places", "25 places", "20 places")
+
+data = ((x35.data, y35.data), (x30.data, y30.data), (x25.data, y25.data), (x20.data, y20.data))
+colors = ("black", "blue", "green", "red")
+groups = ("35 places", "30 places", "25 places", "20 places")
 
 # Create plot
 figure = plotter.figure()

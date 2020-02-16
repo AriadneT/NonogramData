f69ae23d859d69e140683438b9a257182a1fb592
AriadneT <AriadneT@users.noreply.github.com>
2020-02-16 21:08:00 +0100

    [CLEANUP] Simplify addition of extra tests
    Comment out SQL commands that are no longer needed.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ x30 = DataGroup("x", 30)
 y30 = DataGroup("y", 30)
 x20 = DataGroup("x", 20)
 y20 = DataGroup("y", 20)
-DataGroups = [x35, y35, x25, y25, x30, y30, x20, y20]
+DataGroups = [[x35, y35], [x25, y25], [x30, y30], [x20, y20]]
 config = []
 
 with open('Configuration/config.json') as config_file:
@@ -30,34 +30,35 @@ try:
     if db_connection.is_connected():
         db_interaction = db_connection.cursor()
 
-        db_interaction.execute('CREATE DATABASE IF NOT EXISTS nonogram;')
+        #db_interaction.execute('CREATE DATABASE IF NOT EXISTS nonogram;')
 
         db_interaction.execute('USE nonogram;')
 
-        create_line_table = 'CREATE TABLE IF NOT EXISTS line (line_id INTEGER(11) AUTO_INCREMENT PRIMARY KEY, length TINYINT(4) NOT NULL, places TINYINT(4) NOT NULL);'
-        db_interaction.execute(create_line_table)
-        # from_edge refers to blocks from top or left end
-        create_data_table = 'CREATE TABLE IF NOT EXISTS data (data_id INTEGER(11) AUTO_INCREMENT PRIMARY KEY, line_id INTEGER(11) NOT NULL, succession TINYINT(4) NOT NULL, from_edge TINYINT(4) NOT NULL, block_number TINYINT(4) NOT NULL, longest_block TINYINT(4) NOT NULL, FOREIGN KEY (line_id) REFERENCES line(line_id));'
-        db_interaction.execute(create_data_table)
+        #create_line_table = 'CREATE TABLE IF NOT EXISTS line (line_id INTEGER(11) AUTO_INCREMENT PRIMARY KEY, length TINYINT(4) NOT NULL, places TINYINT(4) NOT NULL);'
+        #db_interaction.execute(create_line_table)
+        # From_edge refers to blocks from top or left end
+        #create_data_table = 'CREATE TABLE IF NOT EXISTS data (data_id INTEGER(11) AUTO_INCREMENT PRIMARY KEY, line_id INTEGER(11) NOT NULL, succession TINYINT(4) NOT NULL, from_edge TINYINT(4) NOT NULL, block_number TINYINT(4) NOT NULL, longest_block TINYINT(4) NOT NULL, FOREIGN KEY (line_id) REFERENCES line(line_id));'
+        #db_interaction.execute(create_data_table)
 
         # Entering data via .csv files when table has foreign key does not work
         # unless foreign key ignored, which is not desired
 
         # Lines vary from 20 to 35 in length
-        for data_group in DataGroups:
-            if data_group.type == "y":
-                db_interaction.execute("SELECT succession FROM data JOIN line ON data.line_id = line.line_id WHERE places = " + str(data_group.spaces))
-                order_results = db_interaction.fetchall()
-                for order in order_results:
-                    data_group.data.append(order[0])
-            else:
-                # For numbers below 1, multiply by 100 to allow stats.lineregress to work.
-                # Because line lengths vary and therefore affect maximum block size,
-                # correct for length. Log transformation seems to be more suitable.
-                db_interaction.execute("SELECT log(longest_block / length) * 100 FROM data JOIN line ON data.line_id = line.line_id WHERE places = " + str(data_group.spaces))
-                longest_block_results = db_interaction.fetchall()
-                for longest_block in longest_block_results:
-                    data_group.data.append(int(longest_block[0]))
+        for data_pair in DataGroups:
+            for data_group in data_pair:
+                if data_group.type == "y":
+                    db_interaction.execute("SELECT succession FROM data JOIN line ON data.line_id = line.line_id WHERE places = " + str(data_group.spaces))
+                    order_results = db_interaction.fetchall()
+                    for order in order_results:
+                        data_group.data.append(order[0])
+                else:
+                    # For numbers below 1, multiply by 100 to allow stats.lineregress to work.
+                    # Because line lengths vary and therefore affect maximum block size,
+                    # correct for length. Log transformation seems to be more suitable.
+                    db_interaction.execute("SELECT log(longest_block / length) * 100 FROM data JOIN line ON data.line_id = line.line_id WHERE places = " + str(data_group.spaces))
+                    longest_block_results = db_interaction.fetchall()
+                    for longest_block in longest_block_results:
+                        data_group.data.append(int(longest_block[0]))
 except Error as error:
     print("Error while connecting to MySQL", error)
 finally:
@@ -79,29 +80,16 @@ with open('x_values.txt', 'r') as x_file:
             x_read.append(int(number))
 """
 # Linear regression analysis
-slope20, intercept20, r_value20, p_value20, std_err20 = stats.linregress(x20.data,y20.data)
-slope25, intercept25, r_value25, p_value25, std_err25 = stats.linregress(x25.data,y25.data)
-slope30, intercept30, r_value30, p_value30, std_err30 = stats.linregress(x30.data,y30.data)
-slope35, intercept35, r_value35, p_value35, std_err35 = stats.linregress(x35.data,y35.data)
+for data_pair in DataGroups:
+    slope, intercept, r_value, p_value, std_err = stats.linregress(data_pair[0].data,data_pair[1].data)
 
-with open('Results/linear_regressions.txt', 'w') as data_file:
-    data_file.write("When 20 lines must be filled:\n\n")
-    data_file.write("order = " + str(slope20) + "log(longest_block / length) x 100 + " + str(intercept20) + "\n")
-    data_file.write("R-squared: " + str(r_value20**2) + "\n")
-    data_file.write("P-value: " + str(p_value20) + "\n\n")
-    data_file.write("When 25 lines must be filled:\n\n")
-    data_file.write("order = " + str(slope25) + "log(longest_block / length) x 100 + " + str(intercept25) + "\n")
-    data_file.write("R-squared: " + str(r_value25**2) + "\n")
-    data_file.write("P-value: " + str(p_value25) + "\n\n")
-    data_file.write("When 30 lines must be filled:\n\n")
-    data_file.write("order = " + str(slope30) + "log(longest_block / length) x 100 + " + str(intercept30) + "\n")
-    data_file.write("R-squared: " + str(r_value30**2) + "\n")
-    data_file.write("P-value: " + str(p_value30) + "\n\n")
-    data_file.write("When 35 lines must be filled:\n\n")
-    data_file.write("order = " + str(slope35) + "log(longest_block / length) x 100 + " + str(intercept35) + "\n")
-    data_file.write("R-squared: " + str(r_value35**2) + "\n")
-    data_file.write("P-value: " + str(p_value35) + "\n\n")
-    
+    with open('Results/linear_regressions.txt', 'a') as data_file:
+        data_file.write("When " + str(data_pair[0].spaces) + " lines must be filled:\n\n")
+        data_file.write("order = " + str(slope) + "log(longest_block / length) x 100 + " + str(intercept) + "\n")
+        data_file.write("Standard error: " + str(std_err) + "\n")
+        data_file.write("R-squared: " + str(r_value**2) + "\n")
+        data_file.write("P-value: " + str(p_value) + "\n\n")
+
 data = ((x35.data, y35.data), (x30.data, y30.data), (x25.data, y25.data), (x20.data, y20.data))
 colors = ("black", "blue", "green", "red")
 groups = ("35 places", "30 places", "25 places", "20 places")
